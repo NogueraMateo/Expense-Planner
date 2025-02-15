4dc368fd8d4eff9d27d9b4ffb910881068c7ee2f
NogueraMateo <mateo.edu.co@gmail.com>
2025-02-15 12:10:16 -0500

    feat: initial layout

---
+++ src/components/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div
+    class="alert"
+  >
+    <slot></slot>
+  </div>
+
+</template>
+
+<style scoped>
+  .alert {
+    padding: 1rem;
+    font-weight: 900;
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 1.6rem;
+    background-color: var(--blanco);
+    color: #b91c1c;
+    border-left: .5rem solid #b91c1c;
+    margin-bottom: 2rem;
+}
+</style>
